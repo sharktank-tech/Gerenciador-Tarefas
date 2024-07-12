c2b378cf894fed29f54b897a3b2766799c444492
Danilo Cavalcante <82549409+sharktank-tech@users.noreply.github.com>
2024-07-12 22:55:46 +0000

    initial commit

--- vendas.py
+++
@@ -1,9 +0,0 @@
-import smtplib
-
-server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-server.login("danilokog652@gmail.com", "tydf hpmj opge nqkp")
-server.sendmail(
-  "danilokog652@gmail.com",
-  "natylimas652@gmail.com",
-  "menssagem teste")
-server.quit()
